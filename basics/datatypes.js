825ee4499607dd4bbb61b572427adb7d9bb25fa3
Divyamishra2209 <134643651+Divyamishra2209@users.noreply.github.com>
2025-02-24 10:35:33 +0000

    data types

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all js code as newer version
+//alert('hello')// we are using node js, not browaer 
+console.log(3
+    +
+    3)// code readability should be heigh 
+console.log("divya")
+
+let name = "divya"
+let age = 18
+let isloggedIN = false
+let state;
+let temprature = null
+//number=>2 to power 53
+// bigint (trrading,stock market,big number)
+//string => ""
+//boolean => true/false
+//null standalone value 
+//undefiend =>
+// symbol(unique find karne k liye use hota hai)
+// object
+
+console.log(typeof"divya");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);//o/p undefine
