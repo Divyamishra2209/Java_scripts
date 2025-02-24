4b3e9475f814f448ead031c7008cdf2db7fb5633
Divyamishra2209 <134643651+Divyamishra2209@users.noreply.github.com>
2025-02-24 09:45:38 +0000

    variables java scripts

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountID = 122344
+let ACCOUNT_EMAIL ="divya2gamil.com"
+var accountpasword = "123456"
+accountcity = "Jaipur"
+
+//accountID = 2// not allowed
+ACCOUNT_EMAIL = "@divyagunju"
+accountpasword = "12345677890"
+accountcity = "delhi"
+let accountstate;
+
+console.log(accountID)
+
+/*
+prefer not to use var 
+because of issues in block scope and functional scope */
+
+console.table({accountID,ACCOUNT_EMAIL,accountpasword,accountcity,accountstate})
