cf0328798918606d4a3fce4b8767b887e92c2b71
Divyamishra2209 <134643651+Divyamishra2209@users.noreply.github.com>
2025-03-10 12:07:59 +0000

    conversion

---
+++ basics/conversion_operation.js
@@ -0,0 +1,35 @@
+let Value = 3
+let negvalue = -Value;
+//console.log(negvalue);
+//The error in your code is due to a typo in the variable name. JavaScript is case-sensitive, and you've declared the variable Value with an uppercase "V," but you're trying to assign -value to negvalue, using a lowercase "v."
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); power
+// console.log(2/3);float value output 
+// console.log(2%3);modulo remainder
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+// console.log("1" + 2); converte srting all value
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");add 1+2 convert 2 in string
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
